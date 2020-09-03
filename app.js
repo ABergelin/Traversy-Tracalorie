45522a317b919a2d593ab077ad0b1bfbe51cac71
A Bergelin <the_bergelin@hotmail.co.uk>
2020-09-03 20:47:28 +0100

    Rename function

--- app.js
+++ app.js
@@ -184,7 +184,7 @@ const App = (function (ItemCtrl, UICtrl) {
         document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
 
         // Edit icon click event
-        document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+        document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
     }
 
     // Add item submit
@@ -212,8 +212,8 @@ const App = (function (ItemCtrl, UICtrl) {
         e.preventDefault();
     }
 
-    // Update item submit
-    const itemUpdateSubmit = function (e) {
+    // Click edit item
+    const itemEditClick = function (e) {
         if (e.target.classList.contains('edit-item')) {
             // Get list item id (item-0, item-1)
             const listID = e.target.parentNode.parentNode.id;
